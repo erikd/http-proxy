d89ed3f6b197d43a9c25b96151ca43fcdd1a4856
Erik de Castro Lopo <erikd@mega-nerd.com>
2012-02-02 14:51:04 +1100

    test/Util.hs : Add functionality.

--- test/Util.hs
+++ test/Util.hs
@@ -12,9 +12,60 @@ module Util where
 import Network.HTTP.Types
 
 import Data.ByteString                    (ByteString)
+import Data.String                        (fromString)
 
 import qualified Data.ByteString          as BS
+import qualified Data.ByteString.Char8    as BS8
 import qualified Data.CaseInsensitive     as CI
+import qualified Network.HTTP.Conduit     as HC
+import qualified Network.HTTP.Types       as HT
+import qualified Network.Wai              as Wai
+
+
+dumpWaiRequest :: Wai.Request -> IO ()
+dumpWaiRequest req =
+    let text = BS.concat
+            [ "------- Wai Request --------------------------------------------------------------\n"
+            , "Method          : " , Wai.requestMethod req , "\n"
+            , "HTTP Version    : " , fromString (show (Wai.httpVersion req)) , "\n"
+            , "Path Info       : " , Wai.rawPathInfo req , "\n"
+            , "Query String    : " , Wai.rawQueryString req , "\n"
+            , "Server Name     : " , Wai.serverName req , "\n"
+            , "Server Port     : " , fromString (show (Wai.serverPort req)), "\n"
+            , "Secure (SSL)    : " , fromString (show (Wai.isSecure req)), "\n"
+            , "Remote Host     : " , fromString (show (Wai.remoteHost req)), "\n"
+            , "Request Headers :\n"
+            , headerShow (Wai.requestHeaders req), "\n"
+            ]
+    in BS.putStr text
+
+
+dumpHttpConduitRequest :: HC.Request m -> IO ()
+dumpHttpConduitRequest req =
+    let text = BS.concat
+            [ "------- HttpConduit Request ------------------------------------------------------\n"
+            , "Method          : " , HC.method req , "\n"
+            , "Secure (SSL)    : " , fromString (show (HC.secure req)), "\n"
+            , "Host Name       : " , HC.host req , "\n"
+            , "Host Port       : " , fromString (show (HC.port req)), "\n"
+            , "Path            : " , HC.path req , "\n"
+            , "Query String    : " , HT.urlDecode False (HC.queryString req), "\n"
+            , "Request Headers :\n"
+            , headerShow (HC.requestHeaders req), "\n"
+            ]
+    in BS.putStr text
+
+
+dumpHttpResponse :: HT.Status -> HT.ResponseHeaders -> IO ()
+dumpHttpResponse s rh = do
+    let text = BS.concat
+            [ "------- Response from upsteam ----------------------------------------------------\n"
+            , "HTTP/1.0 ", BS8.pack (show (HT.statusCode s)), " ", HT.statusMessage s, "\n"
+            ]
+
+    BS.putStr text
+    BS.putStr $ BS.concat $ map (\ (f, v) -> BS.concat [ "    ", CI.original f, ": ", v, "\n" ]) rh
+
 
 headerShow :: [Header] -> ByteString
 headerShow headers =
