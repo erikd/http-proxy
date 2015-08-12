a269f73e3f0a9baa22b73a4c2966b28ec8a43d40
Sharif Olorin <sio@tesser.org>
2015-08-12 02:38:37 +0000

    Implement {proxy,wai}Request roundtrip

--- Network/HTTP/Proxy/Request.hs
+++ Network/HTTP/Proxy/Request.hs
@@ -15,8 +15,10 @@ module Network.HTTP.Proxy.Request
     where
 
 import Data.ByteString.Char8 (ByteString)
+
 import Data.Maybe
 
+import           Network.HTTP.Types (Method)
 import qualified Network.HTTP.Types as HT
 import qualified Network.Wai as Wai
 
@@ -27,13 +29,9 @@ type Port = Int
 data Request = Request
     {
     -- | Request method such as GET.
-      requestMethod :: HT.Method
+      requestMethod :: Method
     -- | HTTP version such as 1.1.
     , httpVersion :: HT.HttpVersion
-    -- | The upstream host name.
-    , requestHost :: ByteString
-    -- | The port number of the upstream host.
-    , requestPort :: Port
     -- | A list of header (a pair of key and value) in an HTTP request.
     , requestHeaders :: HT.RequestHeaders
     -- | The part of the URL after the hostname (and optional port number) and
@@ -43,12 +41,27 @@ data Request = Request
     , queryString :: HT.Query
     }
 
-
 proxyRequest :: Wai.Request -> Request
-proxyRequest = error "proxyRequest"
+proxyRequest w = Request method'
+                         version'
+                         headers'
+                         path'
+                         query'
+         where
+    method'  = Wai.requestMethod w
+    version' = Wai.httpVersion w
+    headers' = Wai.requestHeaders w
+    path'    = Wai.rawPathInfo w
+    query'   = Wai.queryString w
 
 waiRequest :: Request -> Wai.Request
-waiRequest = error "waiRequest"
+waiRequest r = Wai.defaultRequest
+    { Wai.requestMethod  = requestMethod r
+    , Wai.httpVersion    = httpVersion r
+    , Wai.requestHeaders = requestHeaders r
+    , Wai.rawPathInfo    = requestPath r
+    , Wai.queryString    = queryString r
+    }
 
 waiRequestHost :: Wai.Request -> ByteString
 waiRequestHost req = fromMaybe "???" $ Wai.requestHeaderHost req
