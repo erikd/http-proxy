c6fd4c008baae3e787899c3d8ae85f7ddcf8693c
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-04-07 10:55:38 +1000

    Fix request-rewrite-proxy example
    And add it to the cabal file.

--- example/request-rewrite-proxy.hs
+++ example/request-rewrite-proxy.hs
@@ -1,14 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Network.HTTP.Proxy (ProxySettings (..), Request (..))
+import qualified Data.ByteString.Char8 as BS
+
+import           Network.HTTP.Proxy (Settings (..), Request (..))
 import qualified Network.HTTP.Proxy as Proxy
+import           Network.URI (URI (..), URIAuth (..), parseURI)
+import           Network.Wai.Internal (Response)
 
 main :: IO ()
 main =
   Proxy.runProxySettings $
     Proxy.defaultProxySettings
       { proxyPort = 31081
-      , proxyHttpRequestModifier = Just secureGoogle
+      , proxyHttpRequestModifier = secureGoogle
       }
 
 -- Modifying the request like this is only possible for unencrypted HTTP connections
@@ -16,12 +20,29 @@ main =
 -- HTTPS cnnections cannot be modified like this because the for HTTPS connections
 -- even the request itself is encrypted.
 
-secureGoogle :: Request -> IO Request
-secureGoogle req
-  | "www.google.com" `BS.isInfixOf` requestPath req
-      && not ("https" `BS.isprefixOf` requestPath req =
-        pure $ req
-                { requestPath = "encrypted.google.com"
-                }
+secureGoogle :: Request -> IO (Either Response Request)
+secureGoogle req = do
+  case parseURI $ BS.unpack (requestPath req) of
+    Nothing -> do
+      putStrLn $ "Not able to parse: " ++ show (requestPath req)
+      -- Not much to be done other than just return the Request unmodified.
+      pure $ Right req
+    Just uri ->
+      pure . Right $ req { requestPath = BS.pack $ show (modifyURI uri) }
 
-  | otherwise = pure req
+modifyURI :: URI -> URI
+modifyURI uri =
+  uri
+    { uriAuthority = modifyUriAthority <$> uriAuthority uri
+    , uriScheme = modifyUriScheme (uriScheme uri)
+    }
+ where
+  modifyUriAthority :: URIAuth -> URIAuth
+  modifyUriAthority auth =
+    if uriRegName auth == "www.google.com"
+      then auth { uriRegName = "encrypted.google.com", uriPort = "" }
+      else auth
+
+  modifyUriScheme :: String -> String
+  modifyUriScheme scheme =
+    if scheme =="http:" then "https:" else scheme
